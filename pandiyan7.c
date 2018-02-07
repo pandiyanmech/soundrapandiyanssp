c0be65eec330d453a4ebcd904b6789924d029bed
pandiyanmech <35764854+pandiyanmech@users.noreply.github.com>
2018-02-07 16:29:56 +0530

    pandiyan7

---
+++ pandiyan7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,sum=0;
+printf("enter the natural number");
+scanf("%d"&n);
+for(a=0;a<=0;a++)
+{
+sum+=0;
+printf("sum of natural numbers %d");
+}
+getch();
+}
