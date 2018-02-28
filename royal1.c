37955be186af71a54429e47c98d965268a825c5f
pandiyanmech <35764854+pandiyanmech@users.noreply.github.com>
2018-02-28 16:13:01 +0530

    royal

---
+++ royal1.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+ 
+int main()
+ 
+{
+ 
+  int n1, n2, i, temp, num, rem;
+ 
+  printf("Enter two numbers(intervals): ");
+ 
+  scanf("%d %d", &n1, &n2);
+ 
+  printf("Armstrong numbers between %d an %d are: ", n1, n2);
+ 
+  for(i=n1+1; i<n2; ++i)
+ 
+  {
+ 
+      temp=i;
+ 
+      num=0;
+ 
+      while(temp!=0)
+ 
+      }
+ 
+          rem=(temp%10);
+ }
